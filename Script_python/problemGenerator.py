c54d3ad28a416ff455de97d7c62e34080d3c8811
alex <monteleone.1883922@studenti.uniroma1.it>
2024-10-20 22:06:39 +0200

    fixed prob 1

--- Script_python/problemGenerator.py
+++ Script_python/problemGenerator.py
@@ -267,30 +267,32 @@ prob.generateInputForDrawing()
 
 ################## ISTANZA 1 NEW ##################
 
-# prob = Problem("new_problem1", 5, 5)
-#
-# # tiles disponibili
-# prob.setTiles3(3)
-# prob.setTiles5(2)
-# prob.setTiles6(2)
-# # prob.setTiles7(1)
-# prob.setTiles9(2)
-# prob.setTilesA(2)
-# prob.setTilesB(2)
-# prob.setTilesC(3)
-# prob.setTilesD(1)
+prob = Problem("new_problem1", 5, 5)
+
+
+
+# tiles disponibili
+prob.setTiles3(3)
+prob.setTiles5(1)
+prob.setTiles6(2)
+# prob.setTiles7(1)
+prob.setTiles9(2)
+prob.setTilesA(1)
+#prob.setTilesB(2)
+prob.setTilesC(3)
+prob.setTilesD(1)
 # prob.setTilesE(1)
-# prob.setTilesF(1)
-#
-# #ori
-# prob.addGold(1,5)
-# prob.addGold(0,2)
-# prob.addGold(3,3)
-#
-# # argenti
-# prob.addSilver(0,3)
-# prob.addSilver(2,1)
-# prob.addSilver(3,6)
+prob.setTilesF(1)
+
+#ori
+prob.addGold(1,4)
+prob.addGold(0,1)
+prob.addGold(3,2)
+
+# argenti
+prob.addSilver(0,2)
+prob.addSilver(2,0)
+prob.addSilver(4,3)
 
 ################## ISTANZA 2 NEW ##################
 
